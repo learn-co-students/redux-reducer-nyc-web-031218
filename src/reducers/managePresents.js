91f87963a87300401e2ae4116d034761a711ae62
Dwire <gjdwyer8@gmail.com>
2018-06-04 20:10:02 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0}, action){
+  // console.log("state:", state);
+  // console.log("action:", action);
+  switch(action.type) {
+    case "INCREASE":
+      return {...state.numberOfPresents, numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
